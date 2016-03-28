4e74d699d55869756fe522a7d64219cfa429119c
Song Younghwan <purluno@gmail.com>
2016-03-29 01:53:12 +0900

    dice: 주사위 굴림 실패 확률 40% -> 45.5%

--- dice/dice7.js
+++ dice/dice7.js
@@ -9,11 +9,11 @@
 var Rx = require('rx')
 
 // 주사위를 한번 던지는 비동기 observable.
-// 1~6의 값을 가질 수 있는데 의도적인 버그로 인해 약 40% 확률로 실패할 수 있다.
+// 1~6의 값을 가질 수 있는데 의도적인 버그로 인해 약 45.5% 확률로 실패할 수 있다.
 // 한번 던지는데 300~350ms 정도 걸린다.
 var diceRoll = Rx.Observable.create(observer => {
   setTimeout(() => {
-    var dice = Math.floor(Math.random() * 10) // 0~9
+    var dice = Math.floor(Math.random() * 11) // 0~10
     if (dice < 1 || 6 < dice) {
       observer.onError(new Error('A roll failed'))
     } else {
